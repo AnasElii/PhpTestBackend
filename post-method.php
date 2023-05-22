ed07e6eee989122bb5832b70b938bb98ac2325b2
Aninoss II <aninosspro@gmial.com>
2023-05-22 00:00:40 +0000

    Json in post Method

--- post-method.php
+++ post-method.php
@@ -2,8 +2,10 @@
 if (isset($_POST['submit'])) {
     $firstname = $_POST["firstname"];
     // $lastname = $_POST["lastname"];
-    echo "<h3>From Post Method</h3>";
-    echo "Your name is: " . $firstname . " | Post.php";
+    header("Context-Type: application/json; charset=UTF-8");
+    echo json_encode($firstname);
+    // echo "<h3>From Post Method</h3>";
+    // echo "Your name is: " . $firstname . " | Post.php";
     // echo "Your name is: " . $firstname . " " . $lastname . " | Post.php";
     exit;
 } else {
